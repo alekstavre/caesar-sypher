8e7c949d5510d19825c1b7d72a9b2438667d22f2
alex stavreski <alex_21@live.com>
2019-03-26 11:23:43 +0100

    last tweaks added features

--- caesar sypher.java
+++ caesar sypher.java
@@ -1,12 +1,9 @@
 package testing;
-
 import java.util.Scanner;
 
-public class TestTwo {
+public class TestOne {
 
-
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
 		String str;
@@ -22,7 +19,7 @@ public class TestTwo {
 			keyVal = Integer.parseInt(key);
 
 		} catch (Exception e) {
-			
+
 			System.out.print("must enter a number!\n");			
 			keyVal = Integer.parseInt(scan.next());
 
@@ -30,32 +27,40 @@ public class TestTwo {
 
 		while(true) {
 
-			System.out.print(" 1.encrypt\n 2.decrypt");
+			System.out.print("1.encrypt\n2.decrypt");
 			String action = scan.next();
 			switch(action) {
 			case "1" :
-				System.out.println("encrypted message: " + encrypt(str, keyVal));
+				System.out.println("encrypted message: " + decrypt(encrypt(str, keyVal),keyVal));
 				break;
 
 			case "2" :
-				System.out.println("decrypted message: " + decrypt(encrypt(str, keyVal),keyVal));
+				System.out.println("decrypted message: " + encrypt(str, keyVal));
 				break;
-				
-			 default:
-		            System.out.println("None selected!");
+
+			default:
+				System.out.println("None selected!");
 			}
 		}
 
 	}
 
-
 	private static String encrypt(String str, int keyVal) {
 		String encrypted = "";
 		for(int i=0;i<str.length();i++) {
 			int letter = str.charAt(i);
-			letter+=(keyVal%26);
-			if (letter>'z') {
-				letter-=26;
+			if (Character.isUpperCase(letter)) {
+				letter+=(keyVal%26);
+				if (letter>'Z') {
+					letter+=26;
+				}
+				else if (Character.isLowerCase(letter)) {
+					letter-=(keyVal%26);
+					if (letter>'z') {
+						letter-=26;
+					}
+				}
+
 			}
 			encrypted+=(char)letter;
 		}
@@ -66,13 +71,24 @@ public class TestTwo {
 		String decrypted = "";
 		for(int i=0;i<str.length();i++) {
 			int letter = str.charAt(i);
-			letter-=(keyVal%26);
-			if (letter<'a') {
-				letter+=26;
+			if (Character.isUpperCase(letter)) {
+				letter-=(keyVal%26);
+				if (letter<'A') {
+					letter-=26;
+				}
+
 			}
-			decrypted+=(char)letter;
+			else if (Character.isLowerCase(letter)) {
+				letter+=(keyVal%26);
+				if (letter<'a') {
+					letter+=26;
+				}
+				decrypted+=(char)letter;
+			}
+			
 		}
 		return decrypted;
 	}
 
 }
+
